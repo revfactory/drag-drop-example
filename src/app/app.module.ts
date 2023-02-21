0e393fca57294f97a433626d0bca522325051293
robin-hwang <robin.hwang@kakaocorp.com>
2023-02-21 10:01:50 +0900

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { DragDropListComponent } from './drag-drop-list/drag-drop-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DragDropListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DragDropModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
---
+++ src/app/drag-drop-list/drag-drop-list.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-drag-drop-list',
+  templateUrl: './drag-drop-list.component.html',
+  styleUrls: ['./drag-drop-list.component.scss']
+})
+export class DragDropListComponent {
+  bucketList = [
+    '베스트셀러 작가되기',
+    '마라톤 완주하기',
+    '기네스 세계 기록 세우기',
+    '번지점프 하기',
+    '스카이다이빙하기',
+    '박사학위받기',
+    '남극방문하기',
+    '아마존 밀림 탐험하기'
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.bucketList, event.previousIndex, event.currentIndex);
+  }
+}
